f6791259b48fddc94e192353ddb7763b6e5c9be1
agatagolonka <69506024+agatagolonka@users.noreply.github.com>
2021-03-31 00:10:14 +0200

    Update OXBoard.java

--- src/main/java/com/github/agatagolonka/ox/OXBoard.java
+++ src/main/java/com/github/agatagolonka/ox/OXBoard.java
@@ -4,13 +4,14 @@ class OXBoard {
 
     private String board;
 
-    public OXBoard(int size) {
+    OXBoard(int size) {
         if (size == 3) {
             this.board = """
                        1| 2| 3 
                        4| 5| 6
                        7| 8| 9
                     """;
+
         } else if (size == 5) {
             this.board = """
                        1| 2| 3| 4| 5
@@ -23,13 +24,16 @@ class OXBoard {
 
     }
 
-    public String getBoard() {
+    String getBoard() {
         return board;
     }
 
-    public void setBoard(Integer toReplace, Character sign) {
+    void setBoard(Integer toReplace, Character sign) {
         if(toReplace>9) {
             this.board = this.board.replace(toReplace.toString(), " "+sign.toString());
+        }
+        else if(toReplace==1||toReplace==2){
+            this.board = this.board.replace(" "+toReplace.toString()+"|", " "+sign.toString()+"|");
         }
         else{
             this.board = this.board.replace(" "+toReplace.toString(), " "+sign.toString());
